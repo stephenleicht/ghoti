80f3839fd69eb22af51355180781624e19accf68
Stephen Leicht <stephen.leicht@gmail.com>
2021-01-23 15:54:47 -0500

    complete more gulp file conversion

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -1,23 +1,11 @@
-// const path = require('path');
-// const gulp = require('gulp');
-// const ts = require('gulp-typescript');
-// const sourcemaps = require('gulp-sourcemaps');
-// const del = require('del');
-// const runSequence = require('run-sequence');
-// const merge = require('merge2');
-// const webpack = require('webpack');
-// const rollup = require('rollup');
-// const rollupTypescript = require('rollup-plugin-typescript2');
-// const rollupUglify = require('rollup-plugin-uglify')
-
 import del from 'del';
-import path from 'path';
 import gulp from 'gulp';
-import ts from 'gulp-typescript';
 import sourcemaps from 'gulp-sourcemaps';
+import ts from 'gulp-typescript';
+import path from 'path';
 import webpack from 'webpack';
-
 import generateWebpackConfig from './webpack.client.mjs';
+
 
 const { series, parallel, src, dest, watch } = gulp;
 
@@ -27,7 +15,6 @@ const files = {
     library: [
         'src/**/*.@(ts|tsx)',
         '!src/admin/client/forms/examples/**/*.*',
-        // '!src/example/**/*/*'
     ],
     packageJsons: 'src/**/package.json',
 };
@@ -46,8 +33,8 @@ export function buildLibrary() {
         .pipe(sourcemaps.init())
         .pipe(tsProject())
         .pipe(sourcemaps.write())
-        .pipe(src(files.packageJsons))
-        .pipe(dest(OUTDIR));     
+        // .pipe(src(files.packageJsons))
+        .pipe(dest(OUTDIR));
 }
 
 export function buildAdminClient(cb) {
@@ -80,72 +67,17 @@ export function buildAdminClient(cb) {
 
 export const build = series(clean, parallel(buildLibrary, buildAdminClient));
 
-export const buildWatch = series(async () => {
-    shouldWatch = true;
-    watch(files.library, buildLibrary)
-    return;
-}, build)
-// gulp.task('release', (cb) => {
-//     isReleaseBuild = true;
-//     runSequence('build', cb);
-// })
+export const buildWatch = series(
+    function initWatch(cb) {shouldWatch = true; cb()},
+    build,
+    function watching(){ watch(files.library, buildLibrary) }
+);
 
-// gulp.task('watch', ['clean'], (cb) => {
-//     shouldWatch = true;
+export const release = series(
+    function setIsRelease(cb) {isReleaseBuild = true; cb()},
+    build,
+);
 
-//     runSequence(['build-library', 'build-admin-client'], () => {
-//         gulp.watch(files.library, ['build-library']);
-//         gulp.watch(files.packageJsons, ['build-library']);
-
-//         cb();
-//     })
-// })
-
-// gulp.task('build-library', () => {
-//     const tsProject = ts.createProject('./tsconfig.json');
-
-//     return merge([
-//         gulp.src(files.library)
-//             .pipe(sourcemaps.init())
-//             .pipe(tsProject())
-//             .pipe(sourcemaps.write())
-//             .pipe(gulp.dest(OUTDIR)),
-
-//         gulp.src(files.packageJsons)
-//             .pipe(gulp.dest(OUTDIR))
-//     ]);
-// })
-
-// gulp.task('build-admin-client', () => runSequence(['bundle-admin-client']));
-
-// gulp.task('bundle-admin-client', (cb) => {
-//     const webpackOptions = {
-//         isReleaseBuild,
-//         outDir: OUTDIR,
-//     }
-
-//     const webpackConfig = require('./webpack.client')(webpackOptions);
-//     const bundler = webpack(webpackConfig);
-
-//     if (!shouldWatch) {
-//         bundler.run((err, stats) => {
-//             console.log(stats.toString());
-//             cb(err);
-//         })
-//     }
-//     else {
-//         let hasCalledBack;
-//         bundler.watch(null, (err, stats) => {
-//             if (!hasCalledBack) {
-//                 cb();
-//                 hasCalledBack = true;
-//             }
-
-//             console.log(stats.toString())
-//         })
-//     }
-
-// });
 
 // gulp.task('release-forms', ['clean'], () => {
 //     gulp.src([
